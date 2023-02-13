3ccedfcae1fe768a044318b115d1e4a19b44d69b
rln <rln@gmail.com>
2023-02-13 20:33:30 +0530

    new commit

---
+++ Fundamentals-of-Terraform/AWS-VPC/v1-vpc-module/c3-vpc.tf
@@ -0,0 +1,52 @@
+#Create VPC Terraform Module
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+  
+  #VPC Basic Details
+  
+  name = "my_Vpc"
+  cidr = "10.0.0.0/16"
+  azs                 = ["us-east-1a", "us-east-1b"]
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets      = ["10.0.11.0/24", "10.0.12.0/24"]
+  
+  # Database Subnets
+
+  create_database_subnet_group = true
+  create_database_subnet_route_table= true
+  database_subnets    = ["10.0.21.0/24", "10.0.22.0/24"]
+
+  #create_database_nat_gateway_route = true
+  #create_database_internet_gateway_route = true
+
+  # NAT Gateways - Outbound Communication
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+   # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
+  tags = {
+    Owner = "RLN"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "My_vpc"
+  }
+}
